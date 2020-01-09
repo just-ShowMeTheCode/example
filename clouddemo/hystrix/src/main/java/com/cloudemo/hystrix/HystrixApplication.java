cf3fdcfc9f6d94823dd33d61b88d6d73561bf06c
fumj <fumj@citydo.com.cn>
2020-01-09 10:08:49 +0800

    1. hystrix 配置

--- clouddemo/hystrix/src/main/java/com/cloudemo/hystrix/HystrixApplication.java
+++ clouddemo/hystrix/src/main/java/com/cloudemo/hystrix/HystrixApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 /**
  * @author fumj
@@ -13,6 +14,7 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
  */
 @SpringCloudApplication
 @EnableHystrixDashboard
+@EnableTurbine
 @EnableCircuitBreaker
 public class HystrixApplication {
     public static void main(String[] args) {
